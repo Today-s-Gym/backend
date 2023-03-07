965dcd828c62a1e2bd79e10f548dcda386180948
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-07 23:07:22 +0900

    chore: 백엔드 프로젝트 생성

---
+++ src/main/java/com/todaysgym/todaysgym/TodaysgymApplication.java
@@ -0,0 +1,13 @@
+package com.todaysgym.todaysgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysgymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysgymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todaysgym/todaysgym/TodaysgymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaysgym.todaysgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
