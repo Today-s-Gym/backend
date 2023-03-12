b787a99fd47fe0409bfd31bbb3643a7e0d0dbcea
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-13 01:25:35 +0900

    feat: Category enum 생성

---
+++ src/main/java/com/todaysgym/todaysgym/category/Category.java
@@ -0,0 +1,11 @@
+package com.todaysgym.todaysgym.category;
+
+public enum Category {
+    NONE("none");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+}
