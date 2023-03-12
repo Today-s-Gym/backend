e67828b19e3eba9aee62e65562ee12d1501762d6
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-13 01:26:49 +0900

    feat: RecordPhoto 엔티티 생성

---
+++ src/main/java/com/todaysgym/todaysgym/record/photo/RecordPhoto.java
@@ -0,0 +1,33 @@
+package com.todaysgym.todaysgym.record.photo;
+
+import com.todaysgym.todaysgym.record.Record;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "record_photo")
+public class RecordPhoto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imgUrl;
+    private String fileName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id")
+    private Record record;
+}
