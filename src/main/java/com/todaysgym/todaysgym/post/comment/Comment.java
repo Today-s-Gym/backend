e5778e009d2fdfe8b940a41403f8674146c5057d
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-13 01:26:16 +0900

    feat: Comment 엔티티 생성

---
+++ src/main/java/com/todaysgym/todaysgym/post/comment/Comment.java
@@ -0,0 +1,39 @@
+package com.todaysgym.todaysgym.post.comment;
+
+import com.todaysgym.todaysgym.member.Member;
+import com.todaysgym.todaysgym.post.Post;
+import com.todaysgym.todaysgym.utils.BaseTimeEntity;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "comment")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    private String content;
+    private int report = 0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
