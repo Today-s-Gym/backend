32f25e17b80c380b14519b675deced5551beb735
Sewoong <lympsw12@naver.com>
2023-03-18 23:18:46 +0900

    feature/ Record[#14]

--- src/main/java/com/todaysgym/todaysgym/record/RecordService.java
+++ src/main/java/com/todaysgym/todaysgym/record/RecordService.java
@@ -1,7 +1,11 @@
 package com.todaysgym.todaysgym.record;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RecordService {
+
+    private final RecordRepository recordRepository;
 }
