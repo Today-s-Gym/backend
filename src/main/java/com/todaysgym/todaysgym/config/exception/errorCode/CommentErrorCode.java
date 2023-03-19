ca9bb6b567e528f89f76a7ef035011b33c395965
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-19 13:08:19 +0900

    fix: ErrorCode 구현하도록 수정

--- src/main/java/com/todaysgym/todaysgym/config/exception/errorCode/CommentErrorCode.java
+++ src/main/java/com/todaysgym/todaysgym/config/exception/errorCode/CommentErrorCode.java
@@ -1,11 +1,12 @@
 package com.todaysgym.todaysgym.config.exception.errorCode;
 
+import com.todaysgym.todaysgym.config.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum CommentErrorCode {
+public enum CommentErrorCode implements ErrorCode {
     EMPTY_COMMENT("COMMENT_001", "존재하지 않는 댓글입니다."),
     ;
     private final String errorCode;
