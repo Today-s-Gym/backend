67fc83ab602d964615eced70ff67f15211a31dc6
Sewoong <lympsw12@naver.com>
2023-03-22 01:14:24 +0900

    Record 에러처리

--- src/main/java/com/todaysgym/todaysgym/record/RecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.todaysgym.todaysgym.record;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
