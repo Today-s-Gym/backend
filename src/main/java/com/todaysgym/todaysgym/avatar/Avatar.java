84756751203f32e32e9162c9188603b9c8b2904f
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-22 12:37:47 +0900

    [FEATURE] 아바타 정보를 생성한다 (#19)
    * application.yml 추가
    
    * Create deploy.yml
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * [FEATURE] 엔티티를 생성한다. (#9) (#10)
    
    * feat: Avatar 엔티티 생성
    
    * feat: Category enum 생성
    
    * feat: Member 엔티티 생성
    
    * feat: Post 엔티티 생성
    
    * feat: Comment 엔티티 생성
    
    * feat: PostLike 엔티티 생성
    
    * feat: PostPhoto 엔티티 생성
    
    * feat: Record 엔티티 생성
    
    * feat: RecordPhoto 엔티티 생성
    
    * feat: Report 엔티티 생성
    
    * feat: Tag 엔티티 생성
    
    * feat: BaseTimeEntity 추상 클래스 생성
    
    * feat: ReportType enum 생성
    
    * chore: category id 추가
    
    * chore: submodule 변경사항
    
    * Readme.md 추가
    
    * deploy.sh 주석 추가 (자동 배포 마무리)
    
    * feat: 아바타 정보 생성
    
    ---------
    
    Co-authored-by: Sewoong <lympsw12@naver.com>

--- src/main/java/com/todaysgym/todaysgym/avatar/Avatar.java
+++ src/main/java/com/todaysgym/todaysgym/avatar/Avatar.java
@@ -1,7 +1,12 @@
 package com.todaysgym.todaysgym.avatar;
 
 public enum Avatar {
-    NONE(0, "none", -1, -1);
+    BBURAKMA1(1, "https://todaysgym-bucket.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+1.png", 0, 3),
+    BBURAKMA2(2, "https://todaysgym-bucket.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+2.png", 3, 10),
+    BBURAKMA3(3, "https://todaysgym-bucket.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+3.png", 10, 20),
+    BBURAKMA4(4, "https://todaysgym-bucket.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+4.png", 20, 35),
+    BBURAKMA5(5, "https://todaysgym-bucket.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+5.png", 35, 60),
+    ;
     private final int level;
     private final String imgUrl;
     private final int minRecordCount;
