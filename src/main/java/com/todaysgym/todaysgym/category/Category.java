98debb3952a60d424571f7cccfc43cc4fb49eb9d
Haena <67584244+EUNJEONGMUN@users.noreply.github.com>
2023-03-22 12:47:23 +0900

    [FEATURE] 카테고리 정보를 생성한다. (#20)
    * application.yml 추가
    
    * Create deploy.yml
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * 자동 배포 설정
    
    * [FEATURE] 엔티티를 생성한다. (#9) (#10)
    
    * feat: Avatar 엔티티 생성
    
    * feat: Category enum 생성
    
    * feat: Member 엔티티 생성
    
    * feat: Post 엔티티 생성
    
    * feat: Comment 엔티티 생성
    
    * feat: PostLike 엔티티 생성
    
    * feat: PostPhoto 엔티티 생성
    
    * feat: Record 엔티티 생성
    
    * feat: RecordPhoto 엔티티 생성
    
    * feat: Report 엔티티 생성
    
    * feat: Tag 엔티티 생성
    
    * feat: BaseTimeEntity 추상 클래스 생성
    
    * feat: ReportType enum 생성
    
    * chore: category id 추가
    
    * chore: submodule 변경사항
    
    * Readme.md 추가
    
    * deploy.sh 주석 추가 (자동 배포 마무리)
    
    * feat: 카테고리 정보 생성
    
    ---------
    
    Co-authored-by: Sewoong <lympsw12@naver.com>

--- src/main/java/com/todaysgym/todaysgym/category/Category.java
+++ src/main/java/com/todaysgym/todaysgym/category/Category.java
@@ -1,7 +1,20 @@
 package com.todaysgym.todaysgym.category;
 
 public enum Category {
-    NONE(0L, "none");
+    GYM(1L, "헬스"),
+    CROSSFIT(2L, "크로스핏"),
+    CLIMBING(3L, "클라이밍"),
+    KARATE(4L, "가라데"),
+    TAEKWONDO(5L, "태권도"),
+    MUAYTHAI(6L, "무에타이"),
+    JIUJITSU(7L, "주짓수"),
+    JEETKUNEDO(8L, "절권도"),
+    WUSHU(9L, "우슈"),
+    KUDO(10L, "쿠도"),
+    PROWRESTLING(11L, "프로레슬링"),
+    KICKBOXING(12L, "킥복싱"),
+    KENDO(13L, "검도"),
+    ;
 
     private final Long id;
     private final String name;
